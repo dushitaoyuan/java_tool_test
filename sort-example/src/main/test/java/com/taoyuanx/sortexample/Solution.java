1ddc611f4099d3a0b9149f5a6fe6a9b581120c14
dushitaoyuan <dushitaoyuan>
2021-03-22 16:58:28 +0800

    add

--- sort-example/src/main/test/java/com/taoyuanx/sortexample/Solution.java
+++ sort-example/src/main/test/java/com/taoyuanx/sortexample/Solution.java
@@ -19,22 +19,37 @@ class Solution {
             res.add(path);
             return;
         }
-        if(open<n){
+        if (open < n) {
             dfs(n, path + "(", res, open + 1, close);
 
         }
-        if(close<open){
+        if (close < open) {
             dfs(n, path + ")", res, open, close + 1);
         }
 
     }
 
+    public int reverse(int x) {
+        boolean flag = x > 0;
+        x = Math.abs(x);
+        int reverse = 0, temp = x, mod = 0;
+
+        while (x > 0) {
+            temp = reverse;
+            mod = x % 10;
+            x /= 10;
+            reverse *= 10;
+            reverse += mod;
+            if (temp != (reverse - mod) / 10) {
+                return 0;
+            }
+        }
+        return flag ? reverse : -reverse;
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.generateParenthesis(3));
-        System.out.println(solution.generateParenthesis(4));
-
+        System.out.println(solution.reverse(123));
     }
 
 
